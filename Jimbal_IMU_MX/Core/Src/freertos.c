bdef2e14982bc56b5a080beff4fd8974a35c5ebd
hsol0212 <hyesol0212@gmail.com>
2026-05-08 17:33:03 +0900

    feat: cubeMX 수정

--- Jimbal_IMU_MX/Core/Src/freertos.c
+++ Jimbal_IMU_MX/Core/Src/freertos.c
@@ -54,46 +54,6 @@ const osThreadAttr_t defaultTask_attributes = {
   .stack_size = 512 * 4,
   .priority = (osPriority_t) osPriorityAboveNormal,
 };
-/* Definitions for myTaskLed */
-osThreadId_t myTaskLedHandle;
-const osThreadAttr_t myTaskLed_attributes = {
-  .name = "myTaskLed",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityLow,
-};
-/* Definitions for myTaskGyro */
-osThreadId_t myTaskGyroHandle;
-const osThreadAttr_t myTaskGyro_attributes = {
-  .name = "myTaskGyro",
-  .stack_size = 1024 * 4,
-  .priority = (osPriority_t) osPriorityNormal3,
-};
-/* Definitions for myTaskMonitor */
-osThreadId_t myTaskMonitorHandle;
-const osThreadAttr_t myTaskMonitor_attributes = {
-  .name = "myTaskMonitor",
-  .stack_size = 512 * 4,
-  .priority = (osPriority_t) osPriorityNormal5,
-};
-/* Definitions for myTaskMag */
-osThreadId_t myTaskMagHandle;
-const osThreadAttr_t myTaskMag_attributes = {
-  .name = "myTaskMag",
-  .stack_size = 256 * 4,
-  .priority = (osPriority_t) osPriorityNormal3,
-};
-/* Definitions for myTaskGimbal */
-osThreadId_t myTaskGimbalHandle;
-const osThreadAttr_t myTaskGimbal_attributes = {
-  .name = "myTaskGimbal",
-  .stack_size = 512 * 4,
-  .priority = (osPriority_t) osPriorityNormal3,
-};
-/* Definitions for GyroReadySem */
-osSemaphoreId_t GyroReadySemHandle;
-const osSemaphoreAttr_t GyroReadySem_attributes = {
-  .name = "GyroReadySem"
-};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -101,11 +61,6 @@ const osSemaphoreAttr_t GyroReadySem_attributes = {
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
-void ledSystemTask(void *argument);
-void gyroSystemTask(void *argument);
-void monitorSystemTask(void *argument);
-void magSystemTask(void *argument);
-void gimbalSystemTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -135,10 +90,6 @@ void MX_FREERTOS_Init(void) {
   /* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
-  /* Create the semaphores(s) */
-  /* creation of GyroReadySem */
-  GyroReadySemHandle = osSemaphoreNew(1, 1, &GyroReadySem_attributes);
-
   /* USER CODE BEGIN RTOS_SEMAPHORES */
   /* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
@@ -155,21 +106,6 @@ void MX_FREERTOS_Init(void) {
   /* creation of defaultTask */
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
-  /* creation of myTaskLed */
-  myTaskLedHandle = osThreadNew(ledSystemTask, NULL, &myTaskLed_attributes);
-
-  /* creation of myTaskGyro */
-  myTaskGyroHandle = osThreadNew(gyroSystemTask, NULL, &myTaskGyro_attributes);
-
-  /* creation of myTaskMonitor */
-  myTaskMonitorHandle = osThreadNew(monitorSystemTask, NULL, &myTaskMonitor_attributes);
-
-  /* creation of myTaskMag */
-  myTaskMagHandle = osThreadNew(magSystemTask, NULL, &myTaskMag_attributes);
-
-  /* creation of myTaskGimbal */
-  myTaskGimbalHandle = osThreadNew(gimbalSystemTask, NULL, &myTaskGimbal_attributes);
-
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -181,111 +117,21 @@ void MX_FREERTOS_Init(void) {
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+#include "ap.h"
 /* USER CODE END Header_StartDefaultTask */
 __weak void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
+  
+  apInit();               // 👈 추가 1: 하드웨어 및 변수 초기화
+  gimbalSystemTask(NULL); // 👈 추가 2: 짐벌 통합 제어 무한루프
+
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
   /* USER CODE END StartDefaultTask */
-}
-
-/* USER CODE BEGIN Header_ledSystemTask */
-/**
-* @brief Function implementing the myTaskLed thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_ledSystemTask */
-__weak void ledSystemTask(void *argument)
-{
-  /* USER CODE BEGIN ledSystemTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END ledSystemTask */
-}
-
-/* USER CODE BEGIN Header_gyroSystemTask */
-/**
-* @brief Function implementing the myTaskGyro thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_gyroSystemTask */
-__weak void gyroSystemTask(void *argument)
-{
-  /* USER CODE BEGIN gyroSystemTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END gyroSystemTask */
-}
-
-/* USER CODE BEGIN Header_monitorSystemTask */
-/**
-* @brief Function implementing the myTaskMonitor thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_monitorSystemTask */
-__weak void monitorSystemTask(void *argument)
-{
-  /* USER CODE BEGIN monitorSystemTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END monitorSystemTask */
-}
-
-/* USER CODE BEGIN Header_magSystemTask */
-/**
-* @brief Function implementing the myTaskMag thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_magSystemTask */
-__weak void magSystemTask(void *argument)
-{
-  /* USER CODE BEGIN magSystemTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END magSystemTask */
-}
-
-/* USER CODE BEGIN Header_gimbalSystemTask */
-/**
-* @brief Function implementing the myTaskGimbal thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_gimbalSystemTask */
-__weak void gimbalSystemTask(void *argument)
-{
-  /* USER CODE BEGIN gimbalSystemTask */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
-  /* USER CODE END gimbalSystemTask */
 }
 
 /* Private application code --------------------------------------------------*/
